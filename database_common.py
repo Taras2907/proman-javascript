74cfd4b6f258ae05b989db5f98b443d4cf61ce71
Taras Katrych <KatrychTaras@gmail.com>
2019-07-24 06:10:17 +0200

    add queries to create and get data from card and board tables

--- database_common.py
+++ database_common.py
@@ -9,10 +9,10 @@ import psycopg2.extras
 def get_connection_string():
     # setup connection string
     # to do this, please define these environment variables first
-    user_name = os.environ.get('PSQL_USER_NAME', 'taras')
-    password = os.environ.get('PSQL_PASSWORD','katamaran29' )
-    host = os.environ.get('PSQL_HOST', 'localhost')
-    database_name = os.environ.get('PSQL_DB_NAME', 'proman')
+    user_name = os.environ.get('PSQL_USER_NAME', )
+    password = os.environ.get('PSQL_PASSWORD', )
+    host = os.environ.get('PSQL_HOST', )
+    database_name = os.environ.get('PSQL_DB_NAME', )
     env_variables_defined = user_name and password and host and database_name
 
     if env_variables_defined:
