8d88281885475871f61f4071480db5c2f57a369b
Korneliusz Chodnikiewicz <korneliuszchodnikiewicz@gmail.com>
2019-07-25 19:28:37 +0200

    Fixed callback bug in loadCards function

--- static/js/dom.js
+++ static/js/dom.js
@@ -1,121 +1,132 @@
 // It uses data_handler.js to visualize elements
-import { dataHandler } from "./data_handler.js";
+import {dataHandler} from "./data_handler.js";
 
 export let dom = {
-    _appendToElement: function (elementToExtend, textToAppend, prepend = false) {
-        // function to append new DOM elements (represented by a string) to an existing DOM element
-        let fakeDiv = document.createElement('div');
-        fakeDiv.innerHTML = textToAppend.trim();
+	_appendToElement: function (elementToExtend, textToAppend, prepend = false) {
+		// function to append new DOM elements (represented by a string) to an existing DOM element
+		let fakeDiv = document.createElement('div');
+		fakeDiv.innerHTML = textToAppend.trim();
 
-        for (let childNode of fakeDiv.childNodes) {
-            if (prepend) {
-                elementToExtend.prependChild(childNode);
-            } else {
-                elementToExtend.appendChild(childNode);
-            }
-        }
+		for (let childNode of fakeDiv.childNodes) {
+			if (prepend) {
+				elementToExtend.prependChild(childNode);
+			} else {
+				elementToExtend.appendChild(childNode);
+			}
+		}
 
-        return elementToExtend.lastChild;
-    },
-    init: function () {
-        localStorage.clear()
+		return elementToExtend.lastChild;
+	},
+	init: function () {
+		localStorage.clear()
 		dom.addBoardEventListener()
-        // This function should run once, when the page is loaded.
+		// This function should run once, when the page is loaded.
 
-    },
-    loadBoards: function () {
-        // retrieves boards and makes showBoards called
+	},
 
-        dataHandler.getBoards(function(boards){
-            console.log(boards);
-            localStorage.setItem('boards', JSON.stringify(boards));
-            dom.loadCards(function(){
-                dom.showBoards(function(){
-                    dom.showCards()
-                });
-            });
-        });
+	loadBoards: function () {
+		// retrieves boards and makes showBoards called
 
-    },
-    showBoards: function (callback) {
-        // shows boards appending them to #boards div
-        // it adds necessary event listeners also
-        let boards = JSON.parse(localStorage.getItem('boards'));
+		dataHandler.getBoards(function (boards) {
+			localStorage.setItem('boards', JSON.stringify(boards));
+			dom.loadCards(function () {
+				dom.showBoards(function () {
+					dom.showCards()
+				});
+			});
+		});
 
-        for (let board of boards) {
-            dom.showNewBoard(board)
-        }
+	},
 
-        callback()
-    },
-    loadCards: function (callback) {
-        // retrieves cards and makes showCards called
+	showBoards: function (callback) {
+		// shows boards appending them to #boards div
+		// it adds necessary event listeners also
+		let boards = JSON.parse(localStorage.getItem('boards'));
 
-        const boards = JSON.parse(localStorage.getItem('boards'));
+		for (let board of boards) {
+			dom.showNewBoard(board)
+		}
 
-        for (let index=0; index<boards.length; index++) {
+		callback()
+	},
 
-            dataHandler.getCardsByBoardId(parseInt(boards[index].id), function(boardCards) {
-                boards[index].cards = boardCards;
+	loadCards: function (callback) {
+		// retrieves cards and makes showCards called
 
-                if (index === boards.length-1) {
-                    localStorage.setItem('boards', JSON.stringify(boards));
-                    callback()
-                }
+		const boards = JSON.parse(localStorage.getItem('boards'));
 
-            });
-        //        dlaczego console.log(JSON.stringify(cards)) nie dziala tutaj - zwraca pusty array
+		for (let index = 0; index < boards.length; index++) {
 
-        }
-    },
-    showCards: function () {
-        // shows the cards of a board
-        // it adds necessary event listeners also
-        let boards = JSON.parse(localStorage.getItem('boards'));
+			dataHandler.getCardsByBoardId(parseInt(boards[index].id), function (boardCards) {
 
+				boards[index].cards = boardCards;
 
-        dom.addEditListenersToBoardHeaders();
-        for (let board of boards) {
+				if (JSON.parse(JSON.stringify(boards)).every(dom.haveCards)) {
+					localStorage.setItem('boards', JSON.stringify(boards));
+					callback()
 
-            let newCardList = '';
-            let inProgressCardList = '';
-            let testingCardList = '';
-            let doneCardList = '';
-            for(let card of board.cards) {
-                if (card.status_id === 'new') {
-                    newCardList += `
+				}
+
+				// if (index === boards.length - 1) {
+				// 	//sprawdzić czy kazdy boards[index] posiada .cards wtedy wywolac zapisanie
+				// 	console.log(JSON.parse(JSON.stringify(boards)));
+				// 	localStorage.setItem('boards', JSON.stringify(boards));
+				// 	callback()
+					// dom.saveBoardOnLocalStorage(boards).then(callback())
+				// }
+
+			});
+		}
+	},
+
+	showCards: function () {
+		// shows the cards of a board
+		// it adds necessary event listeners also
+		let boards = JSON.parse(localStorage.getItem('boards'));
+		dom.addEditListenersToBoardHeaders();
+		for (let board of boards) {
+
+			let newCardList = '';
+			let inProgressCardList = '';
+			let testingCardList = '';
+			let doneCardList = '';
+			for (let card of board.cards) {
+
+				if (card.status_id === 'new') {
+					newCardList += `
                         <div class="card" id="${card.id}">
                             <div class="card-remove"><i class="fas fa-trash-alt"></i></div>
                             <div class="card-title">${card.title}</div>
                         </div>
                         `
-                } else if (card.status_id === 'in progress') {
-                    inProgressCardList += `
+				} else if (card.status_id === 'in progress') {
+					inProgressCardList += `
                         <div class="card" id="${card.id}">
                             <div class="card-remove"><i class="fas fa-trash-alt"></i></div>
                             <div class="card-title">${card.title}</div>
                         </div>
                         `
-                } else if (card.status_id === 'testing') {
-                    testingCardList += `
+				} else if (card.status_id === 'testing') {
+					testingCardList += `
                         <div class="card" id="${card.id}">
                             <div class="card-remove"><i class="fas fa-trash-alt"></i></div>
                             <div class="card-title">${card.title}</div>
                         </div>
                         `
-                }   else if (card.status_id === 'done') {
-                    doneCardList += `
+				} else if (card.status_id === 'done') {
+					doneCardList += `
                         <div class="card" id="${card.id}">
                             <div class="card-remove"><i class="fas fa-trash-alt"></i></div>
                             <div class="card-title">${card.title}</div>
                         </div>
                         `
-                }
-            }   let columns = '';
-                let listOfGroupedCards = [newCardList,inProgressCardList, testingCardList, testingCardList];
-                dataHandler.getBoardStatuses(board.id, listOfGroupedCards, function (statuses ) {
-                for (let status =0; status<statuses.length; status++){
-                    let newCardColumn = `
+				}
+			}
+			let columns = '';
+			let listOfGroupedCards = [newCardList, inProgressCardList, testingCardList, testingCardList];
+			dataHandler.getBoardStatuses(board.id, listOfGroupedCards, function (statuses) {
+				for (let status = 0; status < statuses.length; status++) {
+					let newCardColumn = `
                 <div class="board-column">
                     <div class="board-column-title">
                     <textarea class="mod-list-header" data-statusId="${statuses[status].id}">${statuses[status].title}</textarea></div>
@@ -123,69 +134,72 @@ export let dom = {
                         ${listOfGroupedCards[status]}
                     </div>
                 </div>
-            `;      columns += newCardColumn;
-                }
+            `;
+					columns += newCardColumn;
+				}
 
-                let boardElement = document.querySelector(`[data-boardsid='${board.id}']`);
-                boardElement.insertAdjacentHTML('beforeend',columns);
-                //dom.addEditListenersToStatusHeaders();
-            }); // call adn apply
+				let boardElement = document.querySelector(`[data-boardsid='${board.id}']`);
+				boardElement.insertAdjacentHTML('beforeend', columns);
+				//dom.addEditListenersToStatusHeaders();
+			}); // call adn apply
 
 
-        }
+		}
 
-    },
-    theDragula: function () { // listOfDivsWithData as an argument //listOfDivsWithData == [document.getDocumentById(right), .....]
-        dragula([
-            document.getElementById("1"),
-            document.getElementById("3"),
-            document.getElementById("4")
-        ], {
-            removeOnSpill: true
-        });
-    },
-    addEditListenersToBoardHeaders: function () {
-        let allBoardHeaders = document.querySelectorAll("[data-boardid]");
-        allBoardHeaders.forEach(function (boardHeader) {
-            boardHeader.addEventListener('focusout', function () {
-            let newTitle = boardHeader.value;
-            let boardId = boardHeader.dataset['boardid'];
-            dataHandler.changeBoardTitle(boardId, newTitle)
-            })
-            })
-    },
-    addEditListenersToStatusHeaders: function () {
-        let allStatusHeaders = document.querySelectorAll("[data-statusid]");
-        allStatusHeaders.forEach(function (statusHeader) {
-            statusHeader.addEventListener('focusout', function () {
-                let newTitle = statusHeader.value;
-                console.log(statusHeader.dataset['statusid']);
-                let statusId = statusHeader.dataset['statusid'];
-                dataHandler.changeStatusTitle(statusId, newTitle)
-            })
-        })
-    },
+	},
 
-    addBoard: function() {
-    	let board = {
-    		boardId: 5,
-    		title: 'New board',
+	theDragula: function () { // listOfDivsWithData as an argument //listOfDivsWithData == [document.getDocumentById(right), .....]
+		dragula([
+			document.getElementById("1"),
+			document.getElementById("3"),
+			document.getElementById("4")
+		], {
+			removeOnSpill: true
+		});
+	},
+
+	addEditListenersToBoardHeaders: function () {
+		let allBoardHeaders = document.querySelectorAll("[data-boardid]");
+		allBoardHeaders.forEach(function (boardHeader) {
+			boardHeader.addEventListener('focusout', function () {
+				let newTitle = boardHeader.value;
+				let boardId = boardHeader.dataset['boardid'];
+				dataHandler.changeBoardTitle(boardId, newTitle)
+			})
+		})
+	},
+
+	addEditListenersToStatusHeaders: function () {
+		let allStatusHeaders = document.querySelectorAll("[data-statusid]");
+		allStatusHeaders.forEach(function (statusHeader) {
+			statusHeader.addEventListener('focusout', function () {
+				let newTitle = statusHeader.value;
+				let statusId = statusHeader.dataset['statusid'];
+				dataHandler.changeStatusTitle(statusId, newTitle)
+			})
+		})
+	},
+
+	addBoard: function () {
+		let board = {
+			boardId: 5,
+			title: 'New board',
 			loggedUser: 'kornel',
 			isPrivate: false,
 		};
 
-    	dataHandler.createNewBoard(board);
+		dataHandler.createNewBoard(board);
 
-    	dom.showNewBoard(board)
+		dom.showNewBoard(board)
 
 	},
 
-	addBoardEventListener: function() {
+	addBoardEventListener: function () {
 		document.getElementById('add-board-btn').addEventListener('click', dom.addBoard)
 	},
 
-	showNewBoard: function(board) {
-    	let newBoard = `
+	showNewBoard: function (board) {
+		let newBoard = `
 			<section class="board" id="board-${board.id}">
 				<div class="board-header">
 				<span class="board-title">
@@ -209,5 +223,19 @@ export let dom = {
 		this._appendToElement(document.querySelector('#board-container'), newBoard)
 	},
 
-    // here comes more features
+	saveBoardOnLocalStorage: function (boards) {
+		return new Promise((resolve, reject) => {
+			localStorage.setItem('boards', JSON.stringify(boards))
+		})
+	},
+
+	haveCards: function hasCards(element, index, array) {
+		if (element.cards) {
+			return true
+		} else {
+			return false
+		}
+	}
+
+	// here comes more features
 };
